ce1c0ea9865abce02ccd49a06ea5664118d8c0b0
namuun39 <namar628@mtroyal.ca>
2020-04-01 13:58:35 -0600

    created favorites file

---
+++ pages/favorites.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+                
+    function addToFavorites($selectedMovie) {
+        
+        if (IsLoggedIn() == true) {
+            $favorites = $_SESSION['Favorites'];
+            $checkMovie = $_GET['id'];
+
+            // checking if there is content in favorites, if not add movie
+            if (!isset($favorites)) {
+                echo 'There is no content here... add your favorite movies!';
+                $favorites = [];
+            } else {
+                if ($selectedMovie == $checkMovie) {
+                    echo $checkMovie . 'movie is already in favorites.';
+                    // display the movie
+                }
+                else {
+                    $favorites[] = $selectedMovie;
+                }
+            }
+            $_SESSION['Favorites'] = $favorites;
+        }
+        else {
+            // redirect to login
+        }
+    }
+
+?>
