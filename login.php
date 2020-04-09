9cc4e67c508f584aed1d15264d64b5e9a8c3eace
falla412 <falla412@mtroyal.ca>
2020-04-08 20:03:37 -0600

    changed css link in pages

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
-    //require_once('../includes/db-helper.inc.php');
-    //require_once('../includes/session-helper.inc.php');
-    //require_once('../includes/config.inc.php');
+    //require_once('includes/db-helper.inc.php');
+    //require_once('includes/session-helper.inc.php');
+    //require_once('includes/config.inc.php');
 	
 	$attemptLoginFailed = false;
 	
@@ -23,7 +23,7 @@
     <meta charset = "UTF-8">
     <meta name = "description" content = "Login page for assignment">
     <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
-    <link rel = "stylesheet" type = "text/css" href = "style/login.css">
+    <link rel = "stylesheet" type = "text/css" href = "css/login.css">
 </head>
 <body>
     <div class = "box">
--- registration.php
+++ registration.php
@@ -1,8 +1,8 @@
 <?php
 
-    //require_once('../includes/db-helper.inc.php');
-    //require_once('../includes/session-helper.inc.php');
-    //require_once('../includes/config.inc.php');
+    //require_once('includes/db-helper.inc.php');
+    //require_once('includes/session-helper.inc.php');
+    //require_once('includes/config.inc.php');
 	
 	$incompleteForm = false;
 	$passwordMatch = true;
@@ -45,7 +45,7 @@
     <meta charset = "UTF-8">
     <meta name = "description" content = "Registration page for assignment">
     <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
-    <link rel = "stylesheet" type = "text/css" href = "style/login.css"> 
+    <link rel = "stylesheet" type = "text/css" href = "css/login.css"> 
 </head>
 <body>
     <div class = "box">
