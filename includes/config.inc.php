e86f065797806f2afcc16a96054b6e131ca7cff8
phseguin <psegu239@mtroyal.ca>
2020-04-09 16:34:48 -0600

    Updating the db config for server code

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,8 +1,8 @@
 <?php
 	define('DBHOST', 'localhost');
 	define('DBNAME', 'Assign2');
-	define('DBUSER', 'Server1');
-	define('DBPASS', 'admin');
+	define('DBUSER', 'root');
+	define('DBPASS', '');
 	define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 	
 	$connection = new PDO(DBCONNSTRING, DBUSER, DBPASS);
