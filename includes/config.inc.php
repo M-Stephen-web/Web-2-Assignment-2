f8cd721ae02a578720d8274c82879be6e0124e59
Albert <amoro461@mtroyal.ca>
2020-04-10 15:02:53 -0600

    Updated config file

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,6 +1,6 @@
 <?php
-	define('DBHOST', 'localhost');
-	define('DBNAME', 'movie');
+	define('DBHOST', '35.226.117.208');
+	define('DBNAME', 'Assign2');
 	define('DBUSER', 'root');
 	define('DBPASS', '');
 	define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
