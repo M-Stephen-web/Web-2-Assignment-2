80414428aa3df395f2c8c8ab6d52ba5cbcb6e717
Albert <amoro461@mtroyal.ca>
2020-04-11 14:58:45 -0600

    Completed basics of favorites page

--- favorites.php
+++ favorites.php
@@ -7,16 +7,19 @@
 <head>
     <meta charset='utf-8' />
     <title>Favourites</title>
-    <link rel="stylesheet" href="css/favourites.css">
+    <link rel="stylesheet" href="css/favorites.css">
     <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
     <header class="header" id="favHeader">
-        <h2>Favourites</h2>
-        <a href="index.php" id="homeButton" class="button">Home</a>
+        <div>
+            <a href="index.php" id="homeButton" class="button">Home</a>
+            <h2>Favourites</h2>
+        </div>
+        <button id='removeAllFavorites'>Remove All Favorites</button>
     </header>
-    <section id="favorites">
-        
+    <section id="favoritesBlock">
     </section>
 </body>
+<script src='js/favorites.js'></script>
--- js/favorites.js
+++ js/favorites.js
@@ -1,12 +1,41 @@
+const posterURL = "https://image.tmdb.org/t/p/";
+const removeFavoriteIdURL = "apis/favorite-delete-id.php?movieId=";
+const removeFavoriteAllURL = "apis/favorite-delete-all.php";
+
 document.addEventListener('DOMContentLoaded', e => {
     const favorites = document.querySelector('favorites');
     const favMovies = [];
     getFavorites();
 
-})
+
+    document.querySelector('#removeAllFavorites').addEventListener('click', function (e)
+    {
+        fetch(removeFavoriteAllURL)
+          .then(function (response) {
+            if (response.ok) {
+              return response.json();
+            } else {
+              return Promise.reject({
+                status: response.status,
+                statusText: response.statusText,
+              });
+            }
+          })
+          .then((data) => {
+              if(data.isSuccessful)
+              {
+                let favoritesBlock = document.querySelector('#favoritesBlock');
+                favoritesBlock.style.display = "none";
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    });
+});
 
 function getFavorites(){
-    fetch("movies-favorite.php")
+    fetch("./apis/movies-favorite.php")
         .then(function (response) {
             if (response.ok) {
                 return response.json();
@@ -18,17 +47,92 @@ function getFavorites(){
             }
         })
         .then((data) => {
-            updateStorage(data); //Place favorites into local storage
+            console.log(data);
 
-            favMovies = data; //Set global variable
+            //updateStorage(data); //Place favorites into local storage
 
-            favMovies = sortMovies(movies); //Reset global variable with sorted movies
+            favMovies = data.data; //Set global variable
 
-            showingMovies = movies.slice(); //Set global variable
+            //favMovies = sortMovies(movies); //Reset global variable with sorted movies
 
-            populateDefaultView();
+            populateFavoriteMovies(favMovies);
         })
         .catch(function (error) {
             console.log(error);
         });
+}
+
+function populateFavoriteMovies(favMovies)
+{
+    for (let i = 0; i < favMovies.length; i++)
+    {
+        addFavoriteMovieBlock(favMovies[i]);
+    }
+}
+
+
+
+let favoritesBlock = document.querySelector('#favoritesBlock');
+
+function addFavoriteMovieBlock(movie)
+{
+    console.log(movie);
+
+
+    let div = document.createElement('div');
+    div.classList.add('favoriteMovieBlock');
+    div.setAttribute('id', 'favoriteBlock'+movie.id);
+
+    let removeButton = document.createElement('a');
+    removeButton.classList.add('removeFavoriteButton');
+    removeButton.textContent = 'X';
+
+    removeButton.addEventListener('click', function(e){
+        removeFavorite(movie.id);
+    });
+
+    div.appendChild(removeButton);
+    
+    let a = document.createElement('a');
+    a.setAttribute("href", 'index.php');
+
+    let img = document.createElement('img');
+    img.classList.add('favoriteMovieImage');
+    img.setAttribute("src", posterURL + "w185" + movie.poster_path);
+    a.appendChild(img);
+
+    let title = document.createElement('div');
+    title.classList.add('favoriteMovieTitle');
+    title.textContent = movie.title;
+    a.appendChild(title);
+
+
+    div.appendChild(a);
+
+    favoritesBlock.appendChild(div);
+}
+
+function removeFavorite(movieId)
+{
+    fetch(removeFavoriteIdURL + movieId)
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject({
+            status: response.status,
+            statusText: response.statusText,
+          });
+        }
+      })
+      .then((data) => {
+          if(data.isSuccessful)
+          {
+            let favoriteBlock = document.querySelector('#favoriteBlock'+movieId);
+            favoriteBlock.style.display = "none";
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
