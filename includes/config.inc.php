6e85db62bfd6d9929533d2dafc67eb3c5b855d43
namuun39 <namar628@mtroyal.ca>
2020-04-11 11:36:30 -0600

    minor changes

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,10 +1,8 @@
 <?php
 	define('DBHOST', 'localhost');
 	define('DBNAME', 'movie');
-	//define('DBHOST', '35.226.117.208');
-	//define('DBNAME', 'Assign2');
 	define('DBUSER', 'root');
-	define('DBPASS', '');
+	define('DBPASS', 'admin');
 	define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 	
 	$connection = new PDO(DBCONNSTRING, DBUSER, DBPASS);
