52e29c283f5233b3f0af3ddeb55e4632146de9e2
namuun39 <namar628@mtroyal.ca>
2020-04-11 15:14:50 -0600

    new contents-favorites

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,14 +1,4 @@
 <?php
-<<<<<<< HEAD
-	define('DBHOST', 'localhost');
-	define('DBNAME', 'movie');
-	define('DBUSER', 'root');
-	define('DBPASS', 'admin');
-	define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
-	
-	$connection = new PDO(DBCONNSTRING, DBUSER, DBPASS);
-?>
-=======
 define('DBHOST', 'localhost');
 define('DBNAME', 'Assign2');
 define('DBUSER', 'root');
@@ -16,4 +6,3 @@ define('DBPASS', 'admin');
 define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 
 $connection = new PDO(DBCONNSTRING, DBUSER, DBPASS);
->>>>>>> 80414428aa3df395f2c8c8ab6d52ba5cbcb6e717
