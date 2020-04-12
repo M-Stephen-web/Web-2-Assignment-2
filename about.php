9bdaa0900167f454d8f073f989a12eab4d4a78c7
namuun39 <namar628@mtroyal.ca>
2020-04-12 15:13:56 -0600

    in progress for about.css

--- about.php
+++ about.php
@@ -16,12 +16,15 @@
     ?>
     <h3>About Us</h3>
     <center>
-        <p class="card">
-            the group that built this webpage is comprised of
-            <a href="https://github.com/M-Stephen-web">Matthew Stephen</a>, <a href="https://github.com/amoro461">Albert Morozov</a>, <a href="https://github.com/phseguin">Pascal Seguin</a>,
-            <a href="https://github.com/falla412">Fraser Allan</a>, and <a href="https://github.com/namuun39">Namuun Amarkhuu</a>
+        <p class="card1">
+            The group that built this webpage is comprised of
+            <a href="https://github.com/M-Stephen-web">Matthew Stephen</a> 
+            <a href="https://github.com/amoro461">Albert Morozov</a> 
+            <a href="https://github.com/phseguin">Pascal Seguin</a>
+            <a href="https://github.com/falla412">Fraser Allan</a>
+            <a href="https://github.com/namuun39">Namuun Amarkhuu</a>
         </p>
-        <p class="card">
+        <p class="card2">
             you can find the repo for this project <a href="https://github.com/M-Stephen-web/Web-2-Assignment-2">here</a>
         </p>
     </center>
