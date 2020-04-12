95ba315882d19c9294f9970041a24821c59d570c
namuun39 <namar628@mtroyal.ca>
2020-04-11 18:16:28 -0600

    more css

--- registration.php
+++ registration.php
@@ -58,7 +58,7 @@
     <meta charset = "UTF-8">
     <meta name = "description" content = "Registration page for assignment">
     <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
-    <link rel = "stylesheet" type = "text/css" href = "css/login.css"> 
+    <link rel = "stylesheet" type = "text/css" href = "css/registration.css"> 
 	<script src = "js/registration.js"></script>
 </head>
 
@@ -67,21 +67,17 @@
         <h2>Register</h2>
 		<form method = "post" action = "registration.php">
             <ul>
-				<li><label for = "firstname">First Name:</label></li>
+				<li><label for = "firstname">First Name *</label></li>
 				<li><input type = "text" name = "firstname" placeholder = "John (Required)"<?php if (isset($_POST['firstname'])){ echo 'value = "' . $_POST['firstname'] . '"';}?> id = "first" required></li> 
 				<li><p id = "firsterror">!</p></li> 
-				<li><label for = "lastname">Last Name</label></li>
+				<li><label for = "lastname">Last Name *</label></li>
 				<li><input type = "text" name = "lastname" placeholder = "Doe (Required)" <?php if (isset($_POST['lastname'])){ echo 'value = "' . $_POST['lastname'] . '"';}?> id = "lname" required></li>
 				<li><p id = "lasterror">!</p></li> 
-				<li><label for = "city">City:</label><li>
+				<li><label for = "city">City *</label><li>
 				<li><input type = "text" name = "city" placeholder = "Los Angeles (Required)" <?php if (isset($_POST['city'])){ echo 'value = "' . $_POST['city'] . '"';}?> id = "city" required></li>
 				<li><p id = "cityerror">!</p></li> 
-				<li><select name = "country" id = "country">
-						<option value = "default">Select Country</option>
-						<option value = "Canada" <?php if(isset($_POST['country']) && $_POST['country'] == "Canada"){ echo 'selected';}?>>Canada</option>
-						<option value = "USA">United States of America</option>
-						<option value = "Mexico">Mexico</option>
-				</select></li>
+				<li><label for = "country">Country *</label></li>
+				<li><input type = "text" name = "country" placeholder = "Algeria (Required)" <?php if (isset($_POST['country'])){ echo 'value = "' . $_POST['country'] . '"';}?> id = "country" required></li>
 				<li><p id = "countryerror">!</p></li>
 				<li><label for = "email" id = "eid">Email:</label></li>
 				<li><input type = "email" name = "email" placeholder = "jdoe@mail.com (Required)" <?php if (isset($_POST['email'])){ echo 'value = "' . $_POST['email'] . '"';}?>id = "email" required></li>
@@ -89,7 +85,7 @@
 				<li><label for = "password">Password:</label></li>
 				<li><input type = "password" name = "password" placeholder = "Password (Required)" <?php if (isset($_POST['password'])){ echo 'value = "' . $_POST['password'] . '"';}?>id = "password" required></li>
 				<li><p id = "passerror">!</p></li>
-				<li><label for = "confirmpassword">Confrim Password:</label></li>
+				<li><label for = "confirmpassword">Confirm Password:</label></li>
 				<li><input type = "password" name = "confirmpassword" placeholder = "Confirm Password (Required)" <?php if (isset($_POST['confirmpassword'])){ echo 'value = "' . $_POST['confirmpassword'] . '"';}?>id = "confirm" required></li>
 				<li><p id = "confirmerror">!</p></li>
 				<input type = "submit" name = "register" value = "register" id = "submit">  
