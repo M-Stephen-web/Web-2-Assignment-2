cbd7ae8d6557c202f98b6a7cfa0ded1a10b33083
Matt Stephen <matthewstephen12@gmail.com>
2020-04-11 18:46:15 -0600

    finished about page

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,8 +1,8 @@
 <?php
-	define('DBHOST', '35.226.117.208');
-	define('DBNAME', 'Assign2');
-	define('DBUSER', 'root');
-	define('DBPASS', 'admin');
-	define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
-	
-	$connection = new PDO(DBCONNSTRING, DBUSER, DBPASS);
+define('DBHOST', '35.226.117.208');
+define('DBNAME', 'Assign2');
+define('DBUSER', 'root');
+define('DBPASS', 'admin');
+define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+
+$connection = new PDO(DBCONNSTRING, DBUSER, DBPASS);
